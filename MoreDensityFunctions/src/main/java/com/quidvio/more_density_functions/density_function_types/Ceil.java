41f078465e7648167ec867175a2a1f951d26958d
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-09 16:24:09 -0500

    Create Ceil.java

---
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Ceil.java
@@ -0,0 +1,43 @@
+package com.quidvio.more_density_functions.density_function_types;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.dynamic.CodecHolder;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
+
+public record Ceil(DensityFunction df) implements DensityFunctionTypes.class_6932 {
+
+    private static final MapCodec<Ceil> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.CODEC.fieldOf("input").forGetter(Ceil::df)).apply(instance, (Ceil::new)));
+    public static final CodecHolder<Ceil> CODEC = DensityFunctionTypes.method_41065(MAP_CODEC);
+
+    @Override
+    public DensityFunction input() {
+        return this.df;
+    }
+
+    @Override
+    public double apply(double density) {
+        return Math.ceil(density);
+    }
+
+    @Override
+    public DensityFunction apply(DensityFunctionVisitor visitor) {
+        return new Ceil(this.df.apply(visitor));
+    }
+
+    @Override
+    public double minValue() {
+        return Double.MIN_VALUE;
+    }
+
+    @Override
+    public double maxValue() {
+        return Double.MAX_VALUE;
+    }
+
+    @Override
+    public CodecHolder<? extends DensityFunction> getCodec() {
+        return CODEC;
+    }
+}
