6c88dcf59d023387ea327cc6ed83ece521a5b27f
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-09 17:32:42 -0500

    registered dfs

--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/MoreDensityFunctionsMain.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/MoreDensityFunctionsMain.java
@@ -1,9 +1,6 @@
 package com.quidvio.more_density_functions;
 
-import com.quidvio.more_density_functions.density_function_types.Sine;
-import com.quidvio.more_density_functions.density_function_types.Sqrt;
-import com.quidvio.more_density_functions.density_function_types.XClampedGradient;
-import com.quidvio.more_density_functions.density_function_types.ZClampedGradient;
+import com.quidvio.more_density_functions.density_function_types.*;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -20,5 +17,12 @@ public class MoreDensityFunctionsMain implements ModInitializer {
         Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","z_clamped_gradient"), ZClampedGradient.CODEC.codec());
         Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","sine"), Sine.CODEC.codec());
         Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","sqrt"), Sqrt.CODEC.codec());
+        Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","power"), Power.CODEC.codec());
+        Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","floor"), Floor.CODEC.codec());
+        Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","ceil"), Ceil.CODEC.codec());
+        Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","round"), Round.CODEC.codec());
+        Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","div"), FloorDivision.CODEC.codec());
+        Registry.register(Registry.REGISTRIES.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","mod"), Modulo.CODEC.codec());
+
     }
 }
