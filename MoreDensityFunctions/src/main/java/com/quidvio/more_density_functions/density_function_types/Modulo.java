9ab83872a9777bf2b5bc496d677731a808855998
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-09 17:18:23 -0500

    Fix modulo for DF

--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Modulo.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Modulo.java
@@ -1,44 +1,50 @@
 package com.quidvio.more_density_functions.density_function_types;
 
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.dynamic.CodecHolder;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
 import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 
-public record Modulo(DensityFunction df, int divisor) implements DensityFunctionTypes.class_6932 {
+public record Modulo(DensityFunction arg1, DensityFunction arg2) implements DensityFunction {
 
-    private static final MapCodec<Modulo> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.CODEC.fieldOf("input").forGetter(Modulo::df), Codec.intRange(Integer.MIN_VALUE, Integer.MAX_VALUE).fieldOf("divisor").forGetter(Modulo::divisor)).apply(instance, (Modulo::new)));
+    private static final MapCodec<Modulo> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.CODEC.fieldOf("argument_1").forGetter(Modulo::arg1), DensityFunction.CODEC.fieldOf("argument_2").forGetter(Modulo::arg2)).apply(instance, (Modulo::new)));
     public static final CodecHolder<Modulo> CODEC = DensityFunctionTypes.method_41065(MAP_CODEC);
 
-    @Override
-    public DensityFunction input() {
-        return this.df;
-    }
 
-    public int divisor() {
-        return divisor;
+    @Override
+    public double sample(NoisePos pos) {
+        return this.arg1.sample(pos) % this.arg2.sample(pos);
     }
 
     @Override
-    public double apply(double density) {
-        return density % this.divisor;
+    public void method_40470(double[] ds, class_6911 arg) {
+        arg.method_40478(ds, this);
     }
 
     @Override
     public DensityFunction apply(DensityFunctionVisitor visitor) {
-        return new Modulo(this.df.apply(visitor), this.divisor);
+        return visitor.apply(new Modulo(this.arg1,this.arg2));
+    }
+
+    @Override
+    public DensityFunction arg1() {
+        return arg1;
+    }
+
+    @Override
+    public DensityFunction arg2() {
+        return arg2;
     }
 
     @Override
     public double minValue() {
-        return Double.MIN_VALUE;
+        return Math.min(arg1.minValue(),arg2.minValue());
     }
 
     @Override
     public double maxValue() {
-        return Double.MAX_VALUE;
+        return Math.max(arg1.maxValue(),arg2.maxValue());
     }
 
     @Override
