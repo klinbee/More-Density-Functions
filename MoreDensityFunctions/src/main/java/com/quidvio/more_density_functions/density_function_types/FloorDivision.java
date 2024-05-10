742db6dd08c38dac76b0c2a8d5d794e52e73a9bc
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-09 20:53:36 -0500

    mild renames

--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/FloorDivision.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/FloorDivision.java
@@ -9,7 +9,7 @@ import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 
 public record FloorDivision(DensityFunction dividend, DensityFunction divisor, double maxOutput, double minOutput) implements DensityFunction {
 
-    private static final MapCodec<FloorDivision> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.CODEC.fieldOf("dividend").forGetter(FloorDivision::dividend), DensityFunction.CODEC.fieldOf("divisor").forGetter(FloorDivision::divisor), Codec.doubleRange(Double.MIN_VALUE, Double.MAX_VALUE).fieldOf("max_output").forGetter(FloorDivision::maxOutput), Codec.doubleRange(Double.MIN_VALUE, Double.MAX_VALUE).fieldOf("min_output").forGetter(FloorDivision::minOutput)).apply(instance, (FloorDivision::new)));
+    private static final MapCodec<FloorDivision> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.CODEC.fieldOf("dividend").forGetter(FloorDivision::dividend), DensityFunction.CODEC.fieldOf("divisor").forGetter(FloorDivision::divisor), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("max_output").forGetter(FloorDivision::maxOutput), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("min_output").forGetter(FloorDivision::minOutput)).apply(instance, (FloorDivision::new)));
     public static final CodecHolder<FloorDivision> CODEC = DensityFunctionTypes.method_41065(MAP_CODEC);
 
 
--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Reciprocal.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Reciprocal.java
@@ -9,7 +9,7 @@ import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 
 public record Reciprocal(DensityFunction df, double maxOutput, double minOutput) implements DensityFunctionTypes.class_6932 {
 
-    private static final MapCodec<Reciprocal> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.CODEC.fieldOf("input").forGetter(Reciprocal::df), Codec.doubleRange(Double.MIN_VALUE, Double.MAX_VALUE).fieldOf("max_output").forGetter(Reciprocal::maxOutput), Codec.doubleRange(Double.MIN_VALUE, Double.MAX_VALUE).fieldOf("min_output").forGetter(Reciprocal::minOutput)).apply(instance, (Reciprocal::new)));
+    private static final MapCodec<Reciprocal> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.CODEC.fieldOf("input").forGetter(Reciprocal::df), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("max_output").forGetter(Reciprocal::maxOutput), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("min_output").forGetter(Reciprocal::minOutput)).apply(instance, (Reciprocal::new)));
     public static final CodecHolder<Reciprocal> CODEC  = DensityFunctionTypes.method_41065(MAP_CODEC);
 
     @Override
