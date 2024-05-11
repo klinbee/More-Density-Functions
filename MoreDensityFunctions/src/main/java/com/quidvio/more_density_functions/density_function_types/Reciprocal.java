86406f05338a2b269ba3af6f960be677e81cfa35
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-11 11:54:19 -0500

    Update Reciprocal.java

--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Reciprocal.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Reciprocal.java
@@ -7,9 +7,10 @@ import net.minecraft.util.dynamic.CodecHolder;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
 import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 
-public record Reciprocal(DensityFunction df, double maxOutput, double minOutput) implements DensityFunctionTypes.Unary {
+public record Reciprocal(DensityFunction df, double maxOutput, double minOutput,
+                         double errorVal) implements DensityFunctionTypes.Unary {
 
-    private static final MapCodec<Reciprocal> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.FUNCTION_CODEC.fieldOf("input").forGetter(Reciprocal::df), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("max_output").forGetter(Reciprocal::maxOutput), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("min_output").forGetter(Reciprocal::minOutput)).apply(instance, (Reciprocal::new)));
+    private static final MapCodec<Reciprocal> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.FUNCTION_CODEC.fieldOf("input").forGetter(Reciprocal::df), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("max_output").forGetter(Reciprocal::maxOutput), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("min_output").forGetter(Reciprocal::minOutput), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("error_value").forGetter(Reciprocal::errorVal)).apply(instance, (Reciprocal::new)));
     public static final CodecHolder<Reciprocal> CODEC = DensityFunctionTypes.holderOf(MAP_CODEC);
 
     @Override
@@ -20,22 +21,22 @@ public record Reciprocal(DensityFunction df, double maxOutput, double minOutput)
     @Override
     public double apply(double density) {
         if (density == 0) {
-            return maxOutput;
+            return this.errorVal;
         }
-        double output = 1 / density;
-        if (output > maxOutput) {
-            return maxOutput;
+        double result = 1 / density;
+        if (result > this.maxOutput) {
+            return this.maxOutput;
         }
-        if (output < minOutput) {
-            return minOutput;
+        if (result < this.minOutput) {
+            return this.minOutput;
         }
 
-        return output;
+        return result;
     }
 
     @Override
     public DensityFunction apply(DensityFunctionVisitor visitor) {
-        return new Reciprocal(this.df.apply(visitor), this.maxOutput, this.minOutput);
+        return new Reciprocal(this.df.apply(visitor), this.maxOutput, this.minOutput, this.errorVal);
     }
 
     @Override
