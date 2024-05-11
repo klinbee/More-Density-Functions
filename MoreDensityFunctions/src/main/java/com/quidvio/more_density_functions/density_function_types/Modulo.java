8bb0d1443b270b495170933b950700f6bb2dacc2
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-11 11:53:41 -0500

    Update Modulo.java

--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Modulo.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Modulo.java
@@ -1,20 +1,29 @@
 package com.quidvio.more_density_functions.density_function_types;
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.dynamic.CodecHolder;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
 import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 
-public record Modulo(DensityFunction dividend, DensityFunction divisor) implements DensityFunction {
+public record Modulo(DensityFunction dividend, DensityFunction divisor, double errorVal) implements DensityFunction {
 
-    private static final MapCodec<Modulo> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.FUNCTION_CODEC.fieldOf("dividend").forGetter(Modulo::dividend), DensityFunction.FUNCTION_CODEC.fieldOf("divisor").forGetter(Modulo::divisor)).apply(instance, (Modulo::new)));
+    private static final MapCodec<Modulo> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.FUNCTION_CODEC.fieldOf("dividend").forGetter(Modulo::dividend), DensityFunction.FUNCTION_CODEC.fieldOf("divisor").forGetter(Modulo::divisor), Codec.doubleRange(-Double.MAX_VALUE, Double.MAX_VALUE).fieldOf("error_value").forGetter(Modulo::errorVal)).apply(instance, (Modulo::new)));
     public static final CodecHolder<Modulo> CODEC = DensityFunctionTypes.holderOf(MAP_CODEC);
 
 
     @Override
     public double sample(NoisePos pos) {
-        return this.dividend.sample(pos) % this.divisor.sample(pos);
+
+        int dividendValue = (int) this.dividend.sample(pos);
+        int divisorValue = (int) this.divisor.sample(pos);
+
+        if (divisorValue == 0) {
+            return this.errorVal;
+        }
+
+        return dividendValue % divisorValue;
     }
 
     @Override
@@ -24,7 +33,7 @@ public record Modulo(DensityFunction dividend, DensityFunction divisor) implemen
 
     @Override
     public DensityFunction apply(DensityFunctionVisitor visitor) {
-        return visitor.apply(new Modulo(this.dividend.apply(visitor), this.divisor.apply(visitor)));
+        return visitor.apply(new Modulo(this.dividend.apply(visitor), this.divisor.apply(visitor), this.errorVal));
     }
 
     @Override
