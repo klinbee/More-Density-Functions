0c6148f068eb34bf635c053f0e690aa07c7a5562
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-14 18:00:27 -0500

    max/min value method fixes
    sqrt also has an imaginary check

--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Subtract.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/density_function_types/Subtract.java
@@ -8,47 +8,49 @@ import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 
 public record Subtract(DensityFunction arg1, DensityFunction arg2) implements DensityFunction {
 
-    private static final MapCodec<Subtract> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(DensityFunction.FUNCTION_CODEC.fieldOf("argument1").forGetter(Subtract::arg1), DensityFunction.FUNCTION_CODEC.fieldOf("argument2").forGetter(Subtract::arg2)).apply(instance, (Subtract::new)));
-    public static final CodecHolder<Subtract> CODEC = DensityFunctionTypes.holderOf(MAP_CODEC);
+  private static final MapCodec<Subtract> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance
+      .group(DensityFunction.FUNCTION_CODEC.fieldOf("argument1").forGetter(Subtract::arg1),
+          DensityFunction.FUNCTION_CODEC.fieldOf("argument2").forGetter(Subtract::arg2))
+      .apply(instance, (Subtract::new)));
+  public static final CodecHolder<Subtract> CODEC = DensityFunctionTypes.holderOf(MAP_CODEC);
 
+  @Override
+  public double sample(NoisePos pos) {
+    return this.arg1.sample(pos) - this.arg2.sample(pos);
+  }
 
-    @Override
-    public double sample(NoisePos pos) {
-        return this.arg1.sample(pos) - this.arg2.sample(pos);
-    }
+  @Override
+  public void applyEach(double[] densities, EachApplier applier) {
+    applier.applyEach(densities, this);
+  }
 
-    @Override
-    public void applyEach(double[] densities, EachApplier applier) {
-        applier.applyEach(densities, this);
-    }
+  @Override
+  public DensityFunction apply(DensityFunctionVisitor visitor) {
+    return visitor.apply(new Subtract(this.arg1.apply(visitor), this.arg2.apply(visitor)));
+  }
 
-    @Override
-    public DensityFunction apply(DensityFunctionVisitor visitor) {
-        return visitor.apply(new Subtract(this.arg1.apply(visitor), this.arg2.apply(visitor)));
-    }
+  @Override
+  public DensityFunction arg1() {
+    return arg1;
+  }
 
-    @Override
-    public DensityFunction arg1() {
-        return arg1;
-    }
+  @Override
+  public DensityFunction arg2() {
+    return arg2;
+  }
 
-    @Override
-    public DensityFunction arg2() {
-        return arg2;
-    }
+  @Override
+  public double minValue() {
+    return this.arg1.minValue() - this.arg2.maxValue();
+  }
 
-    @Override
-    public double minValue() {
-        return Math.min(arg1.minValue(), arg2.minValue());
-    }
+  @Override
+  public double maxValue() {
+    return this.arg1.maxValue() - this.arg2.minValue();
+  }
 
-    @Override
-    public double maxValue() {
-        return Math.max(arg1.maxValue(), arg2.maxValue());
-    }
-
-    @Override
-    public CodecHolder<? extends DensityFunction> getCodecHolder() {
-        return CODEC;
-    }
+  @Override
+  public CodecHolder<? extends DensityFunction> getCodecHolder() {
+    return CODEC;
+  }
 }
