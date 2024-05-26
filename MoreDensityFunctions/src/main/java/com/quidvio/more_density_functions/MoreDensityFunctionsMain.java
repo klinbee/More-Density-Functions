19ba7d2370f44dc29a6825d10d2a3d76db381ea7
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-26 13:38:07 -0500

    cleaner main

--- MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/MoreDensityFunctionsMain.java
+++ MoreDensityFunctions/src/main/java/com/quidvio/more_density_functions/MoreDensityFunctionsMain.java
@@ -11,32 +11,33 @@ public class MoreDensityFunctionsMain implements ModInitializer {
     public static final double DEFAULT_ERROR = 0.0;
     public static final double DEFAULT_MAX_OUTPUT = 1.0;
     public static final double DEFAULT_MIN_OUTPUT = -1.0;
+    public static final String NAMESPACE = "more_dfs";
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","x_clamped_gradient"), XClampedGradient.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","z_clamped_gradient"), ZClampedGradient.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","sine"), Sine.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","sqrt"), Sqrt.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","power"), Power.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","floor"), Floor.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","ceil"), Ceil.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","round"), Round.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","signum"), Signum.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","div"), Division.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","floor_div"), FloorDivision.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","mod"), Modulo.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","floor_mod"), FloorModulo.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","reciprocal"), Reciprocal.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","shift_df"), ShiftFunction.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","negate"), Negation.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","subtract"), Subtract.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","x"), XCoord.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","y"), YCoord.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","z"), ZCoord.CODEC.codec());
-        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier("more_dfs","simplex"), SimplexNoise.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"x_clamped_gradient"), XClampedGradient.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"z_clamped_gradient"), ZClampedGradient.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"sine"), Sine.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"sqrt"), Sqrt.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"power"), Power.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"floor"), Floor.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"ceil"), Ceil.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"round"), Round.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"signum"), Signum.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"div"), Division.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"floor_div"), FloorDivision.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"mod"), Modulo.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"floor_mod"), FloorModulo.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"reciprocal"), Reciprocal.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"shift_df"), ShiftFunction.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"negate"), Negation.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"subtract"), Subtract.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"x"), XCoord.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"y"), YCoord.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"z"), ZCoord.CODEC.codec());
+        Registry.register(Registries.DENSITY_FUNCTION_TYPE, new Identifier(NAMESPACE,"simplex_noise"), SimplexNoise.CODEC.codec());
     }
 }
