56f87c76a06444d2f50ef435858d432b22240b61
Quidvio <105707614+klinbee@users.noreply.github.com>
2025-05-09 16:19:58 -0500

    chore: forge/fabric registry for dfs

--- fabric/src/main/java/com/klinbee/moredensityfunctions/MoreDensityFunctionsFabric.java
+++ fabric/src/main/java/com/klinbee/moredensityfunctions/MoreDensityFunctionsFabric.java
@@ -1,6 +1,10 @@
 package com.klinbee.moredensityfunctions;
 
+import com.klinbee.moredensityfunctions.densityfunctions.*;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 
 public class MoreDensityFunctionsFabric implements ModInitializer {
     
@@ -12,7 +16,35 @@ public class MoreDensityFunctionsFabric implements ModInitializer {
         // project.
 
         // Use Fabric to bootstrap the Common mod.
-        Constants.LOG.info("Hello Fabric world!");
-        CommonClass.init();
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"asin"), ArcSine.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"acos"), ArcCosine.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"atan"), ArcTangent.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"ceil"), Ceil.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"cos"), Cosine.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"div"), Divide.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"floor"), Floor.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"floor_div"), FloorDivide.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"floor_mod"), FloorModulo.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"ieee_rem"), IEEERemainder.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"negate"), Negate.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"polar_coords"), PolarCoords.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"power"), Power.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"reciprocal"), Reciprocal.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"rem"), Remainder.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"round"), Round.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"shift_df"), ShiftDensityFunction.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"sigmoid"), Sigmoid.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"signum"), Signum.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"sine"), Sine.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"sqrt"), SquareRoot.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"subtract"), Subtract.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"tan"), Tangent.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"vector_angle"), VectorAngle.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID, "x_clamped_gradient"), XClampedGradient.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"x"), XPos.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"y"), YPos.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"z_clamped_gradient"), ZClampedGradient.CODEC.codec());
+        Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, ResourceLocation.tryBuild(Constants.MOD_ID,"z"), ZPos.CODEC.codec());
+        MoreDensityFunctionsCommon.init();
     }
 }
--- forge/src/main/java/com/klinbee/moredensityfunctions/MoreDensityFunctionsForge.java
+++ forge/src/main/java/com/klinbee/moredensityfunctions/MoreDensityFunctionsForge.java
@@ -1,19 +1,88 @@
 package com.klinbee.moredensityfunctions;
 
+import com.klinbee.moredensityfunctions.densityfunctions.*;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
-@Mod(Constants.MOD_ID)
+@Mod(MoreDensityFunctionsConstants.MOD_ID)
 public class MoreDensityFunctionsForge {
-    
+
+    private static final DeferredRegister<Codec<? extends DensityFunction>> DENSITY_FUNCTIONS = DeferredRegister.create(Registries.DENSITY_FUNCTION_TYPE, Constants.MOD_ID);
+    public static final RegistryObject<Codec<? extends DensityFunction>> ASIN = DENSITY_FUNCTIONS.register("asin", ArcSine.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> ACOS = DENSITY_FUNCTIONS.register("acos", ArcCosine.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> ATAN = DENSITY_FUNCTIONS.register("atan", ArcTangent.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> CEIL = DENSITY_FUNCTIONS.register("ceil", Ceil.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> COS = DENSITY_FUNCTIONS.register("cos", Cosine.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> DIV = DENSITY_FUNCTIONS.register("div", Divide.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> FLOOR = DENSITY_FUNCTIONS.register("floor", Floor.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> FLOOR_DIV = DENSITY_FUNCTIONS.register("floor_div", FloorDivide.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> FLOOR_MOD = DENSITY_FUNCTIONS.register("floor_mod", FloorModulo.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> IEEE_REM = DENSITY_FUNCTIONS.register("ieee_rem", IEEERemainder.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> NEGATE = DENSITY_FUNCTIONS.register("negate", Negate.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> POLAR_COORDS = DENSITY_FUNCTIONS.register("polar_coords", PolarCoords.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> POWER = DENSITY_FUNCTIONS.register("power", Power.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> RECIPROCAL = DENSITY_FUNCTIONS.register("reciprocal", Reciprocal.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> REM = DENSITY_FUNCTIONS.register("rem", Remainder.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> ROUND = DENSITY_FUNCTIONS.register("round", Round.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> SHIFT_DF = DENSITY_FUNCTIONS.register("shift_df", ShiftDensityFunction.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> SIGMOID = DENSITY_FUNCTIONS.register("sigmoid", Sigmoid.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> SIGNUM = DENSITY_FUNCTIONS.register("signum", Signum.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> SINE = DENSITY_FUNCTIONS.register("sine", Sine.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> SQRT = DENSITY_FUNCTIONS.register("sqrt", SquareRoot.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> SUBTRACT = DENSITY_FUNCTIONS.register("subtract", Subtract.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> TAN = DENSITY_FUNCTIONS.register("tan", Tangent.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> VEC_ANGLE = DENSITY_FUNCTIONS.register("vector_angle", VectorAngle.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> XGRAD = DENSITY_FUNCTIONS.register("x_clamped_gradient", XClampedGradient.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> X_POS = DENSITY_FUNCTIONS.register("x", XPos.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> Y_POS = DENSITY_FUNCTIONS.register("y", YPos.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> Z_GRAD = DENSITY_FUNCTIONS.register("z_clamped_gradient", ZClampedGradient.CODEC::codec);
+    public static final RegistryObject<Codec<? extends DensityFunction>> Z_POS = DENSITY_FUNCTIONS.register("z", ZPos.CODEC::codec);
+
     public MoreDensityFunctionsForge() {
-    
+
         // This method is invoked by the Forge mod loader when it is ready
         // to load your mod. You can access Forge and Common code in this
         // project.
-    
+
         // Use Forge to bootstrap the Common mod.
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        DENSITY_FUNCTIONS.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
         Constants.LOG.info("Hello Forge world!");
-        CommonClass.init();
-        
+        MoreDensityFunctionsCommon.init();
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
     }
 }
