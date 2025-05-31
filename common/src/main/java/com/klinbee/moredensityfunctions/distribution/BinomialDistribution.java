9a7eef964cd08fce23c959ff153bcb4bb5aa2b02
Quidvio <105707614+klinbee@users.noreply.github.com>
2025-05-31 14:30:06 -0500

    init: binomial distribution

---
+++ common/src/main/java/com/klinbee/moredensityfunctions/distribution/BinomialDistribution.java
@@ -0,0 +1,46 @@
+package com.klinbee.moredensityfunctions.distribution;
+
+import com.klinbee.moredensityfunctions.util.MDFUtil;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.KeyDispatchDataCodec;
+
+public record BinomialDistribution(int numIterations, double probability) implements RandomDistribution {
+    private static final MapCodec<BinomialDistribution> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(
+            Codec.intRange(0, 1_000_000).fieldOf("num_iterations").forGetter(BinomialDistribution::numIterations),
+            Codec.doubleRange(0, 1).fieldOf("probability").forGetter(BinomialDistribution::probability)
+    ).apply(instance, (BinomialDistribution::new)));
+    public static final KeyDispatchDataCodec<BinomialDistribution> CODEC = KeyDispatchDataCodec.of(MAP_CODEC);
+
+    public double getRandom(long hashedSeed) {
+        return MDFUtil.binomial(numIterations, probability, hashedSeed);
+    }
+
+    @Override
+    public int numIterations() {
+        return numIterations;
+    }
+
+    @Override
+    public double probability() {
+        return probability;
+    }
+
+    public double minValue() {
+        return 0.0D;
+    }
+
+    public double maxValue() {
+        return numIterations;
+    }
+
+    @Override
+    public Codec<? extends RandomDistribution> codec() {
+        return CODEC.codec();
+    }
+
+    public static MapCodec<BinomialDistribution> getMapCodec() {
+        return MAP_CODEC;
+    }
+}
