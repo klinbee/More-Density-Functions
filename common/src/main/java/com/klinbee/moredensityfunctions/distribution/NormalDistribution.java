00f6f57aceb68fb0560c6ef3f1b81ffbe35d118d
Quidvio <105707614+klinbee@users.noreply.github.com>
2025-05-31 14:10:00 -0500

    change: first distribution func!

---
+++ common/src/main/java/com/klinbee/moredensityfunctions/distribution/NormalDistribution.java
@@ -0,0 +1,43 @@
+package com.klinbee.moredensityfunctions.distribution;
+
+import com.klinbee.moredensityfunctions.util.MDFUtil;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.KeyDispatchDataCodec;
+
+public record NormalDistribution(double mean, double stdDev) implements RandomDistribution {
+    private static final MapCodec<NormalDistribution> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(Codec.doubleRange(0, Double.MAX_VALUE).fieldOf("mean").forGetter(NormalDistribution::mean), Codec.doubleRange(0, Double.MAX_VALUE).fieldOf("std_dev").forGetter(NormalDistribution::stdDev)).apply(instance, (NormalDistribution::new)));
+    public static final KeyDispatchDataCodec<NormalDistribution> CODEC = KeyDispatchDataCodec.of(MAP_CODEC);
+
+    public double getRandom(long hashedSeed) {
+        return MDFUtil.normal(mean, stdDev, hashedSeed);
+    }
+
+    @Override
+    public double mean() {
+        return mean;
+    }
+
+    @Override
+    public double stdDev() {
+        return stdDev;
+    }
+
+    public double minValue() {
+        return 0.0D;
+    }
+
+    public double maxValue() {
+        return 1.0D;
+    }
+
+    @Override
+    public Codec<? extends RandomDistribution> codec() {
+        return CODEC.codec();
+    }
+
+    public static MapCodec<NormalDistribution> getMapCodec() {
+        return MAP_CODEC;
+    }
+}
