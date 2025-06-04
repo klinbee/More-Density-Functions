d8c8e5ea8e4360610a162d217eb4ff4ebc4754d8
Quidvio <105707614+klinbee@users.noreply.github.com>
2025-06-04 10:15:16 -0500

    change: log functions!

--- common/src/main/java/com/klinbee/moredensityfunctions/util/MDFUtil.java
+++ common/src/main/java/com/klinbee/moredensityfunctions/util/MDFUtil.java
@@ -224,6 +224,8 @@ public class MDFUtil {
 
     /// Standard Math Functions ///
 
+    public static final double LOG2_E = 1.4426950408889634; // 1/ln(2)
+
     /**
      * StrictMath.floorDiv() but returns 0 if the denominator is 0
      *
