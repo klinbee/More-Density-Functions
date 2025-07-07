1355cda8579831b3d807ecbd3ebcbd8a80786653
Klinbee <imquidvio@gmail.com>
2025-07-07 17:19:22 -0500

    change: profiler improvement

--- common/src/main/java/com/klinbee/moredensityfunctions/densityfunctions/Profiler.java
+++ common/src/main/java/com/klinbee/moredensityfunctions/densityfunctions/Profiler.java
@@ -7,16 +7,16 @@ import net.minecraft.util.KeyDispatchDataCodec;
 import net.minecraft.world.level.levelgen.DensityFunction;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-public record Profiler(DensityFunction arg, int iterations, int warmup, int trials) implements DensityFunction {
+public record Profiler(DensityFunction arg, int iterations, int warmup) implements DensityFunction {
     private static final MapCodec<Profiler> MAP_CODEC = RecordCodecBuilder.mapCodec((instance) -> instance.group(
             DensityFunction.HOLDER_HELPER_CODEC.fieldOf("argument").forGetter(Profiler::arg),
             Codec.intRange(0, Integer.MAX_VALUE).fieldOf("iterations").forGetter(Profiler::iterations),
-            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("warm_up").forGetter(Profiler::warmup),
-            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("trials").forGetter(Profiler::trials)
+            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("warm_up").forGetter(Profiler::warmup)
     ).apply(instance, Profiler::new));
     public static final KeyDispatchDataCodec<Profiler> CODEC = KeyDispatchDataCodec.of(MAP_CODEC);
     private static final Set<Profiler> inactiveProfilers = ConcurrentHashMap.newKeySet();
@@ -28,31 +28,34 @@ public record Profiler(DensityFunction arg, int iterations, int warmup, int tria
         }
 
         System.out.println("\nBeginning Profile of Density Function: " + this);
-        List<Long> warmupTimes = new ArrayList<>();
-        List<Long> iterationTimes = new ArrayList<>();
-        for (int t = 0; t < trials; t++) {
-            if (t > 0) System.gc();
-            long startTime = System.nanoTime();
-            for (int i = 0; i < warmup; i++) {
-                arg.compute(pos);
-            }
+        long[] warmUpTimes = new long[warmup];
+        long[] iterationTimes = new long[iterations];
+        for (int i = 0; i < warmup; i++) {
+            long startWarmUp = System.nanoTime();
+            arg.compute(pos);
             long warmupFinish = System.nanoTime();
-            warmupTimes.add(warmupFinish - startTime);
-
-            for (int j = 0; j < iterations; j++) {
-                arg.compute(pos);
-            }
+            warmUpTimes[i] = warmupFinish - startWarmUp;
+        }
+        System.gc();
+        for (int i = 0; i < iterations; i++) {
+            long startIteration = System.nanoTime();
+            arg.compute(pos);
             long iterationFinish = System.nanoTime();
-            iterationTimes.add(iterationFinish - warmupFinish);
+            iterationTimes[i] = iterationFinish - startIteration;
         }
 
-        double avgWarmup = warmupTimes.stream().mapToLong(Long::longValue).average().orElse(0.0);
-        double avgIteration = iterationTimes.stream().mapToLong(Long::longValue).average().orElse(0.0);
+        double avgWarmUpTime = Arrays.stream(warmUpTimes).average().orElse(0.0);
+        double avgIterationTime = Arrays.stream(iterationTimes).average().orElse(0.0);
 
-        System.out.printf("Profile of %d Trials Complete: \nAvg. Warm-up Time: %.3fms\nAvg. Profile Time: %.3fms\n",
-                trials,
-                avgWarmup / 1_000_000.0D,
-                avgIteration / 1_000_000.0D
+        System.out.printf("Profile of %,d iterations:" +
+                        "\nAvg. Time per Compute (warm-up stage): %.3fns" +
+                        "\nAvg. Time per Compute (profile stage): %.3fns" +
+                        "\nProfile Time: %.3fs" +
+                        "\n",
+                iterations,
+                avgWarmUpTime,
+                avgIterationTime,
+                (avgWarmUpTime * warmup + avgIterationTime * iterations) / (1_000_000_000.0D)
         );
 
         inactiveProfilers.add(this);
@@ -66,7 +69,7 @@ public record Profiler(DensityFunction arg, int iterations, int warmup, int tria
 
     @Override
     public DensityFunction mapAll(Visitor visitor) {
-        return visitor.apply(new Profiler(this.arg, this.iterations, this.warmup, this.trials));
+        return visitor.apply(new Profiler(this.arg, this.iterations, this.warmup));
     }
 
     @Override
