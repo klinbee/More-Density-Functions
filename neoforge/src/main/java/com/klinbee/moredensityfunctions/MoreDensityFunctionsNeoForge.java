fb8a6ce1c68f3b613ff9f81adef55208b9a90f81
Klinbee <imquidvio@gmail.com>
2025-07-24 10:41:45 -0500

    change: shift_df -> shift

--- neoforge/src/main/java/com/klinbee/moredensityfunctions/MoreDensityFunctionsNeoForge.java
+++ neoforge/src/main/java/com/klinbee/moredensityfunctions/MoreDensityFunctionsNeoForge.java
@@ -71,7 +71,7 @@ public class MoreDensityFunctionsNeoForge {
         DENSITY_FUNCTIONS.register("reciprocal", Reciprocal.CODEC::codec);
         DENSITY_FUNCTIONS.register("rem", Remainder.CODEC::codec);
         DENSITY_FUNCTIONS.register("round", Round.CODEC::codec);
-        DENSITY_FUNCTIONS.register("shift_df", ShiftDensityFunction.CODEC::codec);
+        DENSITY_FUNCTIONS.register("shift", ShiftDensityFunction.CODEC::codec);
         DENSITY_FUNCTIONS.register("sigmoid", Sigmoid.CODEC::codec);
         DENSITY_FUNCTIONS.register("signum", Signum.CODEC::codec);
         DENSITY_FUNCTIONS.register("sine", Sine.CODEC::codec);
