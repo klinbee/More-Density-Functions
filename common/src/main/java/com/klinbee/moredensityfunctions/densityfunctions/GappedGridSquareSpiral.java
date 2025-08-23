c60e38d750dc2df615e30ed2cdac918882b893f5
Klinbee <imquidvio@gmail.com>
2025-08-23 14:41:22 -0500

    change: added new functions from Bad Apple Worldgen, with enhanced optimizations!

---
+++ common/src/main/java/com/klinbee/moredensityfunctions/densityfunctions/GappedGridSquareSpiral.java
@@ -0,0 +1,158 @@
+package com.klinbee.moredensityfunctions.densityfunctions;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.util.KeyDispatchDataCodec;
+import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraft.world.level.levelgen.DensityFunctions;
+
+import java.util.Arrays;
+
+public record GappedGridSquareSpiral(int xSize,
+                                     int zSize,
+                                     int spacing,
+                                     DensityFunction[] gridCellArgs,
+                                     DensityFunction oobArg)
+        implements DensityFunction {
+
+    // Conversion between List <-> Array
+    private static final Codec<DensityFunction[]> DENSITY_FUNCTION_ARRAY_CODEC =
+            DensityFunction.HOLDER_HELPER_CODEC.listOf()
+                    .xmap(
+                            list -> list.toArray(new DensityFunction[0]),
+                            Arrays::asList
+                    );
+
+    private static final MapCodec<GappedGridSquareSpiral> MAP_CODEC = RecordCodecBuilder.mapCodec(instance ->
+            instance.group(
+                    Codec.INT.fieldOf("x_size").forGetter(GappedGridSquareSpiral::xSize),
+                    Codec.INT.fieldOf("z_size").forGetter(GappedGridSquareSpiral::zSize),
+                    ExtraCodecs.POSITIVE_INT.fieldOf("spacing").orElse(1).forGetter(GappedGridSquareSpiral::spacing),
+                    DENSITY_FUNCTION_ARRAY_CODEC.fieldOf("grid_cell_args").forGetter(GappedGridSquareSpiral::gridCellArgs),
+                    DensityFunction.HOLDER_HELPER_CODEC.fieldOf("out_of_bounds_argument").orElse(DensityFunctions.constant(-1)).forGetter(GappedGridSquareSpiral::oobArg)
+            ).apply(instance, GappedGridSquareSpiral::create)
+    );
+
+    public static final KeyDispatchDataCodec<GappedGridSquareSpiral> CODEC = KeyDispatchDataCodec.of(MAP_CODEC);
+
+    public static final String NAME = "gapped_grid_square_spiral";
+
+    private static GappedGridSquareSpiral create(int xSize,
+                                                 int zSize,
+                                                 int spacing,
+                                                 DensityFunction[] gridCellArgs,
+                                                 DensityFunction oobArg) {
+        // We add 1 to spacing, because if we want spacing of 1 grid, we need to use a modulo 2, etc.
+        return new GappedGridSquareSpiral(xSize, zSize, spacing + 1, gridCellArgs, oobArg);
+    }
+
+
+    @Override
+    public double compute(DensityFunction.FunctionContext pos) {
+
+        int gridX = StrictMath.floorDiv(pos.blockX(), xSize);
+        int gridZ = StrictMath.floorDiv(pos.blockZ(), zSize);
+
+        // Check if we're on a valid grid point
+        if ((gridX % spacing != 0) || (gridZ % spacing != 0)) {
+            return oobArg.compute(pos);
+        }
+
+        // Scale the grid positions
+        int normalizedGridX = gridX / spacing;
+        int normalizedGridZ = gridZ / spacing;
+
+        int index = getSpiralIndex(normalizedGridX, normalizedGridZ);
+
+        int numFunctions = gridCellArgs.length;
+
+        // >= because array indices
+        if (index >= numFunctions) {
+            return oobArg.compute(pos);
+        }
+
+        DensityFunction arg = gridCellArgs[index];
+        return arg.compute(pos);
+    }
+
+    private static int getSpiralIndex(int spiralX, int spiralZ) {
+        // Index formula doesn't work for origin
+        if (spiralX == 0 && spiralZ == 0) {
+            return 0;
+        }
+
+        // Chebyshev distance from origin is the ring the point is in
+        int ring = StrictMath.max(StrictMath.abs(spiralX), StrictMath.abs(spiralZ));
+
+        // Each ring is 8 * ring, so sum of rings is 8 * sum of consecutive integers n * (n + 1) / 2
+        // Therefore, end_index(ring) = 8 * (ring * (ring + 1)) / 2 -> 4 * ring * (ring + 1)
+        // So starting index should be 1 + end_index(ring - 1) -> 1 + 4 * (ring - 1) * ring
+        int index = 1 + 4 * ring * (ring - 1);
+
+        int indexOffset = getOffsetFromStartIndex(spiralX, spiralZ, ring);
+
+        return index + indexOffset;
+    }
+
+    private static int getOffsetFromStartIndex(int spiralX, int spiralZ, int ring) {
+
+        int positionInRing;
+
+        if (spiralX == ring && spiralZ >= -ring && spiralZ < ring) {
+            // Right edge, moving up
+            positionInRing = spiralZ + ring;
+        } else if (spiralZ == ring && spiralX <= ring && spiralX > -ring) {
+            // Top edge, moving left
+            positionInRing = ring * 2 + (ring - spiralX);
+        } else if (spiralX == -ring && spiralZ <= ring && spiralZ > -ring) {
+            // Left edge, moving down
+            positionInRing = ring * 4 + (ring - spiralZ);
+        } else if (spiralZ == -ring && spiralX >= -ring && spiralX < ring) {
+            // Bottom edge, moving right
+            positionInRing = ring * 6 + (spiralX + ring);
+        } else {
+            throw new IllegalArgumentException("Invalid coordinate (" + spiralX + ", " + spiralZ + ") for spiral");
+        }
+
+        return positionInRing;
+    }
+
+    @Override
+    public void fillArray(double[] densities, DensityFunction.ContextProvider applier) {
+        applier.fillAllDirectly(densities, this);
+    }
+
+    @Override
+    public DensityFunction mapAll(DensityFunction.Visitor visitor) {
+
+        // `.mapAll()` cannot be applied to `gridCellArgs` or else, it will lag out Minecraft
+
+        return visitor.apply(
+                new GappedGridSquareSpiral(
+                        xSize,
+                        zSize,
+                        spacing,
+                        gridCellArgs,
+                        oobArg.mapAll(visitor)
+
+                )
+        );
+    }
+
+    @Override
+    public double minValue() {
+        return StrictMath.min(oobArg.minValue(), 0.0D);
+    }
+
+    @Override
+    public double maxValue() {
+        return StrictMath.max(oobArg.maxValue(), 255.0D);
+    }
+
+    @Override
+    public KeyDispatchDataCodec<? extends DensityFunction> codec() {
+        return CODEC;
+    }
+}
---
+++ common/src/main/java/com/klinbee/moredensityfunctions/densityfunctions/SingleChannelImageTessellation.java
@@ -0,0 +1,85 @@
+package com.klinbee.moredensityfunctions.densityfunctions;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.util.KeyDispatchDataCodec;
+import net.minecraft.world.level.levelgen.DensityFunction;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.zip.InflaterInputStream;
+
+public record SingleChannelImageTessellation(int xSize,
+                                             int zSize,
+                                             byte[] inflatedFrameData)
+        implements DensityFunction {
+
+    private static final MapCodec<SingleChannelImageTessellation> MAP_CODEC = RecordCodecBuilder.mapCodec(instance ->
+            instance.group(
+                    Codec.INT.fieldOf("x_size").forGetter(SingleChannelImageTessellation::xSize),
+                    Codec.INT.fieldOf("z_size").forGetter(SingleChannelImageTessellation::zSize),
+                    ExtraCodecs.BASE64_STRING.fieldOf("deflated_frame_data").forGetter(SingleChannelImageTessellation::inflatedFrameData)
+            ).apply(instance, SingleChannelImageTessellation::create)
+    );
+
+    public static final KeyDispatchDataCodec<SingleChannelImageTessellation> CODEC = KeyDispatchDataCodec.of(MAP_CODEC);
+
+    public static final String NAME = "single_channel_image_tessellation";
+
+    private static SingleChannelImageTessellation create(int xSize,
+                                                         int zSize,
+                                                         byte[] deflatedFrameData) {
+        byte[] inflatedBytes;
+        try {
+            var inflaterStream = new InflaterInputStream(new ByteArrayInputStream(deflatedFrameData));
+            inflatedBytes = inflaterStream.readAllBytes();
+        } catch (IOException e) {
+            throw new UncheckedIOException("Error: More Density Functions failed to decompress frame data", e);
+        }
+        return new SingleChannelImageTessellation(xSize, zSize, inflatedBytes);
+    }
+
+    @Override
+    public double compute(FunctionContext pos) {
+        int x = pos.blockX();
+        int z = pos.blockZ();
+        int arrayPos = StrictMath.floorMod(x, xSize) + StrictMath.floorMod(z, zSize) * xSize;
+
+        return inflatedFrameData[arrayPos];
+    }
+
+    @Override
+    public void fillArray(double[] densities, DensityFunction.ContextProvider applier) {
+        applier.fillAllDirectly(densities, this);
+    }
+
+    @Override
+    public DensityFunction mapAll(DensityFunction.Visitor visitor) {
+        return visitor.apply(
+                new SingleChannelImageTessellation(
+                        xSize,
+                        zSize,
+                        inflatedFrameData
+                )
+        );
+    }
+
+    @Override
+    public double minValue() {
+        return 0.0D;
+    }
+
+    @Override
+    public double maxValue() {
+        return 255.0D;
+    }
+
+    @Override
+    public KeyDispatchDataCodec<? extends DensityFunction> codec() {
+        return CODEC;
+    }
+}
+
