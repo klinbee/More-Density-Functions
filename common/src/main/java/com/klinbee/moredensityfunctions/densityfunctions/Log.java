3b47678b4c143e8284e628c3fcf195433edf6ed1
Klinbee <imquidvio@gmail.com>
2025-08-26 10:58:30 -0500

    base log min/max value accuracy

--- common/src/main/java/com/klinbee/moredensityfunctions/densityfunctions/Log.java
+++ common/src/main/java/com/klinbee/moredensityfunctions/densityfunctions/Log.java
@@ -3,6 +3,7 @@ package com.klinbee.moredensityfunctions.densityfunctions;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.KeyDispatchDataCodec;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.levelgen.DensityFunction;
 
 public record Log(DensityFunction arg,
@@ -21,7 +22,7 @@ public record Log(DensityFunction arg,
 
     public static final String NAME = "log";
 
-    private static double eval(double density) {
+    private static double eval(double density1, double density2) {
         return StrictMath.sinh(density);
     }
 
@@ -45,17 +46,103 @@ public record Log(DensityFunction arg,
         );
     }
 
-    // TODO: base it off of base & max input; also use 0/1/negative cases
+    private enum BaseRange {
+        ZERO_TO_ONE_EXCLUSIVE,
+        ONE_TO_INFINITY_EXCLUSIVE,
+        INVALID,
+    }
+
+    private static BaseRange findBaseRange(double min, double max) {
+
+        // Range is inside the (0, 1) set
+        if ((min > 0.0D) && (min < 1.0D) && (max > 0.0D) && (max < 1.0D)) {
+            return BaseRange.ZERO_TO_ONE_EXCLUSIVE;
+        }
+
+        // Range is inside the (1, +∞) set
+        if (min > 1.0D) {
+            return BaseRange.ONE_TO_INFINITY_EXCLUSIVE;
+        }
+
+        // Range is across sets
+        return BaseRange.INVALID;
+    }
+
     @Override
     public double minValue() {
-        return Double.NEGATIVE_INFINITY;
+        double asymptoteLocation = 0.0D;
+
+        double baseMin = base.minValue();
+        double baseMax = base.maxValue();
+
+        return switch (findBaseRange(baseMin, baseMax)) {
+            case ZERO_TO_ONE_EXCLUSIVE -> { // Inverted Log Logic
+                // Since this function is decreasing, the upper bound will always be the minimum
+                double argMax = arg.maxValue();
+
+                // argMax in the set (0, 1) and (1, +∞) produce different logic for min, so doing this instead
+                yield StrictMath.min(eval(argMax, baseMin), eval(argMax, baseMax));
+            }
+            case ONE_TO_INFINITY_EXCLUSIVE -> { // Normal Log Logic
+                // Lower bound is above `asymptoteLocation`, `minValue()` must be `eval(argMin)`
+                double argMin = arg.minValue();
+
+                // argMin in the set (0, 1) and (1, +∞) produce different logic for min, so doing this instead
+                if (argMin > asymptoteLocation) {
+                    yield StrictMath.min(eval(argMin, baseMin), eval(argMin, baseMax));
+                }
+
+                // Upper bound is below `asymptoteLocation`, `minValue()` must be `NaN`
+                double argMax = arg.maxValue();
+
+                if (argMax < asymptoteLocation) {
+                    yield Double.NaN;
+                }
+
+                // Range includes `asymptoteLocation`, `minValue()` cannot be guaranteed, but could be as low as `eval(asymptoteLocation)`
+                yield Double.NEGATIVE_INFINITY;
+            }
+            case INVALID -> Double.NaN; // Cannot determine upper or lower bound
+        };
     }
 
     @Override
     public double maxValue() {
-        return Double.POSITIVE_INFINITY;
-    }
+        double asymptoteLocation = 0.0D;
 
+        double baseMin = base.minValue();
+        double baseMax = base.maxValue();
+
+        return switch (findBaseRange(baseMin, baseMax)) {
+            case ZERO_TO_ONE_EXCLUSIVE -> { // Inverted Log Logic
+                // Lower bound is above `asymptoteLocation`, `maxValue()` must be `eval(argMin)`
+                double argMin = arg.minValue();
+
+                // argMin in the set (0, 1) and (1, +∞) produce different logic for max, so doing this instead
+                if (argMin > asymptoteLocation) {
+                    yield StrictMath.max(eval(argMin, baseMin), eval(argMin, baseMax));
+                }
+
+                // Upper bound is below `asymptoteLocation`, `maxValue()` must be `NaN`
+                double argMax = arg.maxValue();
+
+                if (argMax < asymptoteLocation) {
+                    yield Double.NaN;
+                }
+
+                // Range includes `asymptoteLocation`, `maxValue()` cannot be guaranteed, but could be as high as `eval(asymptoteLocation)`
+                yield Double.POSITIVE_INFINITY;
+            }
+            case ONE_TO_INFINITY_EXCLUSIVE -> { // Normal Log Logic
+                // Since this function is increasing, the upper bound will always be the maximum
+                double argMax = arg.maxValue();
+
+                // argMax in the set (0, 1) and (1, +∞) produce different logic for max, so doing this instead
+                yield StrictMath.min(eval(argMax, baseMin), eval(argMax, baseMax));
+            }
+            case INVALID -> Double.NaN; // Cannot determine upper or lower bound
+        };
+    }
     @Override
     public KeyDispatchDataCodec<? extends DensityFunction> codec() {
         return CODEC;
