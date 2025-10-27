e508506a7a5b1f755c0dbf8a1d964e0a1a63b515
Klinbee <imquidvio@gmail.com>
2025-10-27 14:43:51 -0500

    fix: update to 1.21.10

--- common/src/main/java/com/klinbee/moredensityfunctions/mixin/ChunkMapMixin.java
+++ common/src/main/java/com/klinbee/moredensityfunctions/mixin/ChunkMapMixin.java
@@ -4,8 +4,8 @@ import com.klinbee.moredensityfunctions.randomsamplers.RandomSampler;
 import com.mojang.datafixers.DataFixer;
 import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.progress.ChunkProgressListener;
 import net.minecraft.util.thread.BlockableEventLoop;
+import net.minecraft.world.level.TicketStorage;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.chunk.LightChunkGetter;
 import net.minecraft.world.level.entity.ChunkStatusUpdateListener;
@@ -22,7 +22,7 @@ import java.util.function.Supplier;
 @Mixin(ChunkMap.class)
 public class ChunkMapMixin {
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void MoreDensityFunctions_ChunkMap_captureWorldSeed(ServerLevel level, LevelStorageSource.LevelStorageAccess storageAccess, DataFixer dataFixer, StructureTemplateManager structureTemplateManager, Executor executor, BlockableEventLoop blockableEventLoop, LightChunkGetter $$6, ChunkGenerator $$7, ChunkProgressListener $$8, ChunkStatusUpdateListener $$9, Supplier $$10, int $$11, boolean $$12, CallbackInfo ci) {
+    private void MoreDensityFunctions_ChunkMap_captureWorldSeed(ServerLevel level, LevelStorageSource.LevelStorageAccess storageSource, DataFixer fixerUpper, StructureTemplateManager structureManager, Executor dispatcher, BlockableEventLoop mainThreadExecutor, LightChunkGetter lightChunk, ChunkGenerator generator, ChunkStatusUpdateListener chunkStatusListener, Supplier overworldDataStorage, TicketStorage ticketStorage, int serverViewDistance, boolean sync, CallbackInfo ci) {
         RandomSampler.WorldSeedHolder.setWorldSeed(level.getSeed());
     }
 }
